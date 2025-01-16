74e4add53dc823e9351a34c7853dbaa4b8b23b74
Justin Zhang <justzhang@drwholdings.com>
2025-01-16 12:06:42 -0600

    prune keymaps and use function keys

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -2,27 +2,15 @@ local keymap = vim.keymap.set
 
 vim.g.mapleader = " "
 
--- "kj" to escape
--- keymap({"i", "c"}, "kj", "<Esc>", { desc = "Exit" })
--- keymap("t", "kj", "<C-\\><C-n>", { desc = "Exit" })
+-- reload config
+keymap("n", "<leader>l", "<cmd>so ~/.config/nvim/init.lua<CR>", { desc = "Reload config" })
 
 -- remove highlighting
 keymap("n", "<leader>nl", "<cmd>nohl<CR>", { desc = "Remove highlighting" })
 
--- de-indent in insert mode
-keymap("i", "<S-Tab>", "", { desc = "De-indent" })
-
--- easier dd
-keymap("n", "<leader>d", "dd", { desc = "Easier dd: delete one line" })
-
 -- insert line before/after
 keymap("n", "<CR>", "o<Esc>", { desc = "Add one line below" })
 keymap("n", "<leader><CR>", "O<Esc>", { desc = "Add one line above" })
-
--- comment with <C-/>
-keymap("i", "", "<Esc>gcci<C-f>", { remap = true, desc = "Comment current line" })
-keymap("n", "", "gcc", { remap = true, desc = "Comment current line" })
-keymap("v", "", "gc", { remap = true, desc = "Comment block" })
 
 -- replace current word
 keymap("n", "<leader>r", [[:%s/\<<C-r><C-w>\>//gI<Left><Left><Left>]], { desc = "Replace current word" })
@@ -31,58 +19,41 @@ keymap("n", "<leader>r", [[:%s/\<<C-r><C-w>\>//gI<Left><Left><Left>]], { desc = 
 keymap("n", "<leader>q", "<cmd>wa<CR><cmd>qa<CR>", { desc = "Save and quit all buffers" })
 
 -- better scrolling
-keymap(
-    "n",
-    "<C-d>",
-    "<C-d>zz",
-    { desc = "Jump"  }
-)
-keymap(
-    "n",
-    "<C-u>",
-    "<C-u>zz",
-    { desc = "Jump"  }
-)
+keymap( "n", "<C-d>", "<C-d>zz", { desc = "Jump"  })
+keymap( "n", "<C-u>", "<C-u>zz", { desc = "Jump"  })
 
 -- native remaps
 keymap("n", "h", "'", { desc = "Jump to mark" })
 keymap("n", "l", "@", { desc = "Run macro" })
 
--- leap
+-- leap.nvim
 keymap({"n", "x", "o"}, "s", "<Plug>(leap)", { desc = "Leap" })
 keymap({"n", "x", "o"}, "S", "<Plug>(leap-from-window)", { desc = "Leap from window" })
--- keymap({"x", "o"}, "s", "<Plug>(leap-forward)", { desc = "Leap forward" })
--- keymap({"x", "o"}, "S", "<Plug>(leap-backward)", { desc = "Leap backward" })
 
--- jumplist
-keymap("n", "<leader>cj", "<cmd>clearjumps<CR>", { desc = "Clear jumplist" })
-
--- quickfix jumping
+-- quickfix menu
 keymap("n", "<leader>ce", "<cmd>cn<CR>", { desc = "Go to next quickfix" })
 keymap("n", "<leader>cn", "<cmd>cp<CR>", { desc = "Go to prev quickfix" })
 keymap("n", "<leader>cq", "<cmd>ccl<CR>", { desc = "Close quickfix window" })
 
+-- .h/.cpp file switching
+keymap("n", "<leader>u", "<cmd>ClangdSwitchSourceHeader<CR>", { desc = "Switch between .h/.cpp files" })
+
 -- neotree
 keymap("n", "<leader>ti", "<cmd>Neotree<CR>", { desc = "Open Neotree" })
--- keymap("n", "<leader>ti", "<cmd>NvimTreeOpen<CR>", { desc = "Open Neotree" })
 
 -- telescope
 keymap("n", "<leader>f", "<cmd>Telescope find_files<CR>", { desc = "Open Telescope" })
 keymap("n", "<leader>F", "<cmd>Telescope git_files<CR>", { desc = "Open Telescope with files tracked by Git" })
 keymap("n", "<leader>/", "<cmd>Telescope live_grep<CR>", { desc = "Search for a string with Telescope" })
-keymap("n", "<leader>?", "<cmd>Telescope grep_string<CR>", { desc = "Search for current string with Telescope" })
 keymap("n", "<leader>.", "<cmd>Telescope grep_string<CR>", { desc = "Search for current string with Telescope" })
-
--- .h/.cpp file switching
-keymap("n", "<leader>u", "<cmd>ClangdSwitchSourceHeader<CR>", { desc = "Switch between .h/.cpp files" })
 
 -- harpoon
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
-keymap("n", "<leader>hg", ui.toggle_quick_menu, { desc = "Toggle Harpoon menu" })
+keymap("n", "<leader>ht", ui.toggle_quick_menu, { desc = "Toggle Harpoon menu" })
 keymap("n", "<leader>ha", function() mark.toggle_file(mark.get_current_index()) end, { desc = "Toggle file to Harpoon" })
-keymap("n", "<leader>ge", ui.nav_next, { desc = "Go to next Harpoon mark" })
-keymap("n", "<leader>gn", ui.nav_prev, { desc = "Go to previous Harpoon mark" })
+keymap("n", "<leader>te", ui.nav_next, { desc = "Go to next Harpoon mark" })
+keymap("n", "<leader>tn", ui.nav_prev, { desc = "Go to previous Harpoon mark" })
 
 -- git
 keymap("n", "gh", "<cmd>Gitsigns preview_hunk_inline<CR>", { desc = "Preview hunk" })
@@ -96,34 +67,9 @@ keymap("n", "cn", "<cmd>GitConflictChooseNone<CR>", { desc = "Merge conflict: ch
 keymap("n", "[x", "<cmd>GitConflictPrevConflict<CR>", { desc = "Merge conflict: go to previous" })
 keymap("n", "]x", "<cmd>GitConflictNextConflict<CR>", { desc = "Merge conflict: go to next" })
 
--- open terminal below, or toggle focus to it
-local toggle_terminal = function()
-    -- if focus is on a terminal, move focus away
-    if string.find(vim.fn.bufname(), "term://", 1, true) then
-        vim.cmd("wincmd p")
-        return
-    end
-
-    -- if tab has a terminal, move focus to it
-    for _, win in pairs(vim.api.nvim_tabpage_list_wins(0)) do
-        bufname = vim.fn.bufname(vim.fn.winbufnr(win))
-        if string.find(bufname, "term://", 1, true) then
-            vim.api.nvim_set_current_win(win)
-            vim.cmd("startinsert")
-            return
-        end
-    end
-
-    -- no terminal found, create new terminal
-    vim.cmd("wincmd s")
-    vim.cmd("terminal")
-    vim.cmd("wincmd 7-")
-    vim.cmd("startinsert")
-end
-
-keymap({ "n", "t" }, "<C-!>", toggle_terminal, { desc = "Toggle terminal" })
+-- terminal
 keymap("t", "<C-a>",  "<C-\\><C-n>", { desc = "Escape in terminal" })
-keymap("n", "<leader>tr", ":term<CR>i", { desc = "Open terminal in current window" })
+keymap("n", "<leader>tm", ":term<CR>i", { desc = "Open terminal in current window" })
 
 -- windows and splitting
 keymap("n", "<leader>sm", "<cmd>set nosplitright<CR><C-w>v<cmd>set splitright<CR>", { desc = "Split window vertically left" })
@@ -131,19 +77,27 @@ keymap("n", "<leader>si", "<C-w>v", { desc = "Split window vertically right" })
 keymap("n", "<leader>sn", "<C-w>s", { desc = "Split window horizontally below" })
 keymap("n", "<leader>se", "<cmd>set nosplitbelow<CR><C-w>s<cmd>set splitbelow<CR>", { desc = "Split window horizontally above" })
 keymap("n", "<leader>;", "<C-w>q", { desc = "Close window", remap = true })
-keymap("n", "<leader>w", "<C-w>", { desc = "Easier <C-w>" })
 keymap("n", "<leader>wm", "<C-w>h", { desc = "Easier <C-w>h" })
 keymap("n", "<leader>wn", "<C-w>j", { desc = "Easier <C-w>j" })
 keymap("n", "<leader>we", "<C-w>k", { desc = "Easier <C-w>k" })
 keymap("n", "<leader>wi", "<C-w>l", { desc = "Easier <C-w>l" })
 
+-- windows and splitting, with function keys
+keymap("n", "<F6>", "<C-w>h", { desc = "Easier <C-w>h" })
+keymap("n", "<F7>", "<C-w>j", { desc = "Easier <C-w>j" })
+keymap("n", "<F8>", "<C-w>k", { desc = "Easier <C-w>k" })
+keymap("n", "<F9>", "<C-w>l", { desc = "Easier <C-w>l" })
+keymap("n", "<F10>", "<C-w>q", { desc = "Close window", remap = true })
+keymap("n", "<S-F4>", "<cmd>set nosplitright<CR><C-w>v<cmd>set splitright<CR>", { desc = "Split window vertically left" })
+keymap("n", "<S-F5>", "<C-w>s", { desc = "Split window horizontally below" })
+keymap("n", "<S-F6>", "<cmd>set nosplitbelow<CR><C-w>s<cmd>set splitbelow<CR>", { desc = "Split window horizontally above" })
+keymap("n", "<S-F7>", "<C-w>v", { desc = "Split window vertically right" })
+-- TODO: get F20 to work to close window
+
 -- tabs
-keymap("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })
-keymap("n", "<leader>t;", "<cmd>tabclose<CR>", { desc = "Close current tab" })
-keymap("n", "<leader>tn", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
-keymap("n", "<leader>te", "<cmd>tabn<CR>", { desc = "Go to next tab" })
-keymap("n", "<leader>tO", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
-keymap("n", "<leader>tm", ":tabm ", { desc = "Move tab" })
+keymap("n", "<F11>", "<cmd>tabclose<CR>", { desc = "Close current tab" })
 keymap("n", "<F12>", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
 keymap("n", "<S-F1>", "<cmd>tabn<CR>", { desc = "Go to next tab" })
-keymap("n", "<F11>", "<cmd>tabclose<CR>", { desc = "Close current tab" })
+keymap("n", "<S-F2>", "<cmd>tabnew<CR>", { desc = "Open new tab" })
+keymap("n", "<C-F2>", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
+keymap("n", "<S-F3>", ":tabm ", { desc = "Move tab" })
